85be5c7fcfdb4a04bae62473e0e6e7d7be8e5df2
Darren Shepherd <darren@rancher.com>
2016-11-10 15:18:24 -0700

    Initial Commit

---
+++ package/entry.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+set -e -x
+
+while ! curl -s -f http://rancher-metadata/2015-12-19/stacks/Kubernetes/services/kubernetes/uuid; do
+    echo Waiting for metadata
+    sleep 1
+done
+
+UUID=$(curl -s http://rancher-metadata/2015-12-19/stacks/Kubernetes/services/kubernetes/uuid)
+ACTION=$(curl -s -u $CATTLE_ACCESS_KEY:$CATTLE_SECRET_KEY "$CATTLE_URL/services?uuid=$UUID" | jq -r '.data[0].actions.certificate')
+KUBERNETES_URL=${KUBERNETES_URL:-https://kubernetes:6443}
+
+if [ -n "$ACTION" ]; then
+    mkdir -p /etc/kubernetes/ssl
+    cd /etc/kubernetes/ssl
+    curl -s -u $CATTLE_ACCESS_KEY:$CATTLE_SECRET_KEY -X POST $ACTION > certs.zip
+    unzip -o certs.zip
+    cd $OLDPWD
+
+    cat > /etc/kubernetes/ssl/kubeconfig << EOF
+apiVersion: v1
+kind: Config
+clusters:
+- cluster:
+    api-version: v1
+    certificate-authority: /etc/kubernetes/ssl/ca.pem
+    server: "$KUBERNETES_URL"
+  name: "Default"
+contexts:
+- context:
+    cluster: "Default"
+    user: "Default"
+  name: "Default"
+current-context: "Default"
+users:
+- name: "Default"
+  user:
+    client-certificate: /etc/kubernetes/ssl/cert.pem
+    client-key: /etc/kubernetes/ssl/key.pem
+EOF
+fi
+
+if [ "$1" == "kubelet" ]; then
+    mount --rbind /host/dev /dev
+    FQDN=$(hostname --fqdn || hostname)
+    exec "$@" --hostname-override ${FQDN}
+elif [ "$1" == "kube-apiserver" ]; then
+    CONTAINERIP=$(curl -s http://rancher-metadata/2015-12-19/self/container/ips/0)
+    exec "$@" "--advertise-address=$CONTAINERIP"
+else
+    exec "$@"
+fi
