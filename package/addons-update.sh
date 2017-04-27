088f39df8ec15a2de78704c9004aff8e77834f30
James Oliver <joliver@rancher.com>
2017-04-26 23:08:24 -0700

    manage heapster with helm

--- package/addons-update.sh
+++ package/addons-update.sh
@@ -6,7 +6,9 @@ if [ ${DISABLE_ADDONS} == "true" ]; then
     sleep infinity
 fi
 
-while ! kubectl --kubeconfig=/etc/kubernetes/ssl/kubeconfig --namespace=kube-system get ns kube-system >/dev/null 2>&1; do
+export KUBECONFIG=/etc/kubernetes/ssl/kubeconfig
+
+while ! kubectl --namespace=kube-system get ns kube-system >/dev/null 2>&1; do
 #  echo "Waiting for kubernetes API to come up..."
   sleep 2
 done
@@ -19,15 +21,39 @@ metadata:
       namespace: "kube-system"
 EOF
 
-kubectl --kubeconfig=/etc/kubernetes/ssl/kubeconfig create -f /tmp/rancher-service-account.yaml || true 
+kubectl create -f /tmp/rancher-service-account.yaml || true 
 
 GCR_IO_REGISTRY=${REGISTRY:-gcr.io}
 DOCKER_IO_REGISTRY=${REGISTRY:-docker.io}
+INFLUXDB_HOST_PATH=${INFLUXDB_HOST_PATH:-}
+INFLUXDB_RETENTION=${INFLUXDB_RETENTION:-7d}
 
 for f in $(find /etc/kubernetes/addons -name '*.yaml'); do
   sed -i "s/\$GCR_IO_REGISTRY/$GCR_IO_REGISTRY/g" ${f}
   sed -i "s/\$DOCKER_IO_REGISTRY/$DOCKER_IO_REGISTRY/g" ${f}
-  kubectl --kubeconfig=/etc/kubernetes/ssl/kubeconfig --namespace=kube-system replace --force -f ${f}
+  sed -i "s/\$INFLUXDB_HOST_PATH/$INFLUXDB_HOST_PATH/g" ${f}
+  sed -i "s/\$INFLUXDB_RETENTION/$INFLUXDB_RETENTION/g" ${f}
+  kubectl --namespace=kube-system replace --force -f ${f}
+done
+
+while ! helm version >/dev/null 2>&1; do
+# echo "Waiting fro Helm API to become reachable..."
+  sleep 2
+done
+
+for d in $(ls -d -1 /etc/kubernetes/helm-addons/*); do
+  if [ -f $d/values.yaml ]; then
+    sed -i "s/\$GCR_IO_REGISTRY/$GCR_IO_REGISTRY/g" $d/values.yaml
+    sed -i "s/\$DOCKER_IO_REGISTRY/$DOCKER_IO_REGISTRY/g" $d/values.yaml
+    sed -i "s/\$INFLUXDB_HOST_PATH/$INFLUXDB_HOST_PATH/g" $d/values.yaml
+    sed -i "s/\$INFLUXDB_RETENTION/$INFLUXDB_RETENTION/g" $d/values.yaml
+  fi
+  name=$(basename $d)
+  if [ ! "$(helm ls $name | grep $name)" ]; then
+    helm install --namespace=kube-system -n $name $d
+  else
+    helm upgrade $name $d
+  fi
 done
 
 sleep infinity
