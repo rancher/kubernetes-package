e9ce57313cfc632dbc035dba89d9f956a06a71d6
Alena Prokharchyk <alena@rancher.com>
2017-07-18 10:03:47 -0700

    dns template to package repo
    Also configurable scale and anti affinity
    for skydns service

--- package/addons-update.sh
+++ package/addons-update.sh
@@ -42,7 +42,7 @@ EOF
 GCR_IO_REGISTRY=${REGISTRY:-gcr.io}
 DOCKER_IO_REGISTRY=${REGISTRY:-docker.io}
 INFLUXDB_RETENTION=${INFLUXDB_RETENTION:-0s}
-
+DNS_REPLICAS=${DNS_REPLICAS:-1}
 
 INFLUXDB_HOST_PATH=${INFLUXDB_HOST_PATH:-}
 if [ "$INFLUXDB_HOST_PATH" == "" ]; then
@@ -56,6 +56,7 @@ for f in $(find /etc/kubernetes/addons -name '*.yaml'); do
   sed -i "s|\$DOCKER_IO_REGISTRY|$DOCKER_IO_REGISTRY|g" ${f}
   sed -i "s|\$INFLUXDB_VOLUME|$INFLUXDB_VOLUME|g" ${f}
   sed -i "s|\$INFLUXDB_RETENTION|$INFLUXDB_RETENTION|g" ${f}
+  sed -i "s|\$DNS_REPLICAS|$DNS_REPLICAS|g" ${f}
   kubectl --namespace=kube-system replace --force -f ${f}
 done
 
