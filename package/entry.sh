99ebc24d007b994e6dd77672fff5bf605288a881
moelsayed <m.elsayed@gmail.com>
2018-04-10 00:08:32 +0200

    Fix cadvisor issue

--- package/entry.sh
+++ package/entry.sh
@@ -84,6 +84,7 @@ if [ "$1" == "kubelet" ]; then
     done
     mount --rbind /host/dev /dev
     mount -o rw,remount /sys/fs/cgroup 2>/dev/null || true
+    ln -s /sys/fs/cgroup/cpu,cpuacct /sys/fs/cgroup/cpuacct,cpu 2>/dev/null || true
     for i in /sys/fs/cgroup/*; do
         if [ -d $i ]; then
              mkdir -p $i/kubepods
