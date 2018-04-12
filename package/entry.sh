164957d49562dd2fb34230b0bb6a9759389bd53d
moelsayed <m.elsayed@gmail.com>
2018-04-12 21:03:36 +0200

    Safer way to make cpuacct,cpu symlink

--- package/entry.sh
+++ package/entry.sh
@@ -84,8 +84,11 @@ if [ "$1" == "kubelet" ]; then
     done
     mount --rbind /host/dev /dev
     mount -o rw,remount /sys/fs/cgroup 2>/dev/null || true
-    [ -d /sys/fs/cgroup/cpuacct,cpu ] && rm -rf  /sys/fs/cgroup/cpuacct,cpu
-    ln -s /sys/fs/cgroup/cpu,cpuacct /sys/fs/cgroup/cpuacct,cpu 2>/dev/null || true
+    if ! [ /sys/fs/cgroup/cpu,cpuacct -ef /sys/fs/cgroup/cpuacct,cpu ]
+    then 
+      rm -rf  /sys/fs/cgroup/cpuacct,cpu
+      ln -s /sys/fs/cgroup/cpu,cpuacct /sys/fs/cgroup/cpuacct,cpu 2>/dev/null || true
+    fi
     for i in /sys/fs/cgroup/*; do
         if [ -d $i ]; then
              mkdir -p $i/kubepods
