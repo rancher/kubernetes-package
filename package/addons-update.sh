688c1db0288affc0efe6548f8327abb3e490eb28
moelsayed <m.elsayed@gmail.com>
2018-05-30 02:44:32 +0200

    Revert using types for addons versions

--- package/addons-update.sh
+++ package/addons-update.sh
@@ -67,21 +67,20 @@ else
   INFLUXDB_VOLUME="hostPath:\n          path: $INFLUXDB_HOST_PATH"
 fi
 
+# Addons Images
+# If any of these versions are updated, please also update them in
+# addon-templates/README.md
 
 ADDONS_DIR=/etc/kubernetes/addons
-# image list generate from rancher/types
-# https://github.com/rancher/types/blob/master/apis/management.cattle.io/v3/k8s_defaults.go
-source addons_image_list
-# rancher/types has the images with registry namepsace. Since we have that configurable here, we need to remove it:
-TILLER_IMAGE=${TILLER_IMAGE#*/}
-KUBEDNS_IMAGE=${KUBEDNS_IMAGE#*/}
-DNSMASQ_IMAGE=${DNSMASQ_IMAGE#*/}
-GRAFANA_IMAGE=${GRAFANA_IMAGE#*/}
-HEAPSTER_IMAGE=${HEAPSTER_IMAGE#*/}
-INFLUXDB_IMAGE=${INFLUXDB_IMAGE#*/}
-DASHBOARD_IMAGE=${DASHBOARD_IMAGE#*/}
-DNS_SIDECAR_IMAGE=${DNS_SIDECAR_IMAGE#*/}
 
+DASHBOARD_IMAGE=kubernetes-dashboard-amd64:v1.8.3
+KUBEDNS_IMAGE=k8s-dns-kube-dns-amd64:1.14.8
+DNSMASQ_IMAGE=k8s-dns-dnsmasq-nanny-amd64:1.14.8
+DNS_SIDECAR_IMAGE=k8s-dns-sidecar-amd64:1.14.8
+GRAFANA_IMAGE=heapster-grafana-amd64:v4.4.3
+HEAPSTER_IMAGE=heapster-amd64:v1.5.0
+INFLUXDB_IMAGE=heapster-influxdb-amd64:v1.3.3
+TILLER_IMAGE=tiller:v2.8.2
 
 for f in $(find $ADDONS_DIR -name '*.yaml'); do
   sed -i "s|\$DOCKER_IO_REGISTRY|$DOCKER_IO_REGISTRY|g" ${f}
