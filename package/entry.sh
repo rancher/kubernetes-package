0f2196842e6492a178558a6a8db91d33f9779a9b
moelsayed <m.elsayed@gmail.com>
2018-07-20 22:09:59 +0200

    Apply fix to kubelet only

--- package/entry.sh
+++ package/entry.sh
@@ -102,13 +102,11 @@ if [ "$1" == "kubelet" ]; then
              mkdir -p $i/kubepods
         fi
     done
+    mkdir -p /sys/fs/cgroup/cpuacct,cpu/
+    mount --bind /sys/fs/cgroup/cpu,cpuacct/ /sys/fs/cgroup/cpuacct,cpu/
+    mkdir -p /sys/fs/cgroup/net_prio,net_cls/
+    mount --bind /sys/fs/cgroup/net_cls,net_prio/ /sys/fs/cgroup/net_prio,net_cls/
 fi
-
-mkdir -p /sys/fs/cgroup/cpuacct,cpu/
-mount --bind /sys/fs/cgroup/cpu,cpuacct/ /sys/fs/cgroup/cpuacct,cpu/
-mkdir -p /sys/fs/cgroup/net_prio,net_cls/
-mount --bind /sys/fs/cgroup/net_cls,net_prio/ /sys/fs/cgroup/net_prio,net_cls/
-
 
 FQDN=$(hostname --fqdn || hostname)
 
